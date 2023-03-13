6ed0aa92260f9a48a2896d8b8f57e667225d152d
Saku Salo <saku.salo@live.fi>
2023-03-13 22:05:30 +0200

    Add master container logic skeleton

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -11,6 +11,11 @@ app.config.from_object(Config)
 
 PORT_NUMBER = -1
 NAME = ""
+ID = -1
+LEADER_NAME = ""
+LEADER_PORT = -1
+IS_LEADER = False
+
 # Read port number and name from text file, created in Dockerfile
 with open("port.txt") as f:
     #print(f.readline(), flush=True)
@@ -23,10 +28,28 @@ def register_container(port_number):
     # Basic headers
     headers = {"Content-type": "text/html; charset=UTF-8"}
     # Send the POST-request with container information to database container
-    requests.post(url, data="{},{}".format(NAME, PORT_NUMBER), headers=headers)
+    ID = int(requests.post(url, data="{},{}".format(NAME, PORT_NUMBER), headers=headers).text)
+    print(ID, flush=True)
+
+def get_master_container():
+    global IS_LEADER
+    # docker-compose provides a DNS so we can use database, instead of 127.0.0.1
+    url = "http://database:3003/leader"
+    # Basic headers
+    headers = {"Content-type": "text/html; charset=UTF-8"}
+    # Send the POST-request with container information to database container
+    LEADER_NAME, LEADER_PORT = requests.post(url, data="", headers=headers).text.split(",")
+    print(LEADER_NAME, flush=True)
+    print(LEADER_PORT, flush=True)
+    if NAME == LEADER_NAME and PORT_NUMBER == LEADER_PORT:
+        IS_LEADER = True
+    print(IS_LEADER, flush=True)
 
 # Register container to database
 register_container(port_number=PORT_NUMBER)
+
+# Ask database for master containers information
+get_master_container()
 
 # Import at the bottom to prevent circular imports
 from app import routes
--- server/app/routes.py
+++ server/app/routes.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 # first app = package name (directory app/),
 # second app = Flask class name (variable defined in __init__.py)
-from app import app
+from app import app, PORT_NUMBER, ID, LEADER_PORT, LEADER_NAME, IS_LEADER
 
 # Register callbacks with URLs
 @app.route("/")
 @app.route("/index")
 def index():
     return "Hello, World!"
+
+def send_logging_post_req(data):
+    # URL for database, check port number from /DS2023_CLB/docker-compose.yml for changes
+    # URL needs include protocol
+    # docker-compose provides a DNS so we can use database, instead of 127.0.0.1
+    url = "http://database:3003/logs"
+    # Basic headers
+    headers = {"Content-type": "text/html; charset=UTF-8"}
+    # Send the POST-request with log data to database container
+    requests.post(url, data=data, headers=headers)
